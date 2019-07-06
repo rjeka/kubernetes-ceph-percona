5c6bfaf3e0ade24e8e7725429a6ac8abafcb8830
Evgeniy Rodionov <r_jela@mail.ru>
2019-07-07 02:19:33 +0300

    fix etcd hostname

--- create-config.sh
+++ create-config.sh
@@ -9,20 +9,20 @@
 export K8SHA_IP1=10.73.71.25
 
 # master02 ip address
-export K8SHA_IP2=10.73.71.25
+export K8SHA_IP2=10.73.71.26
 
 # master03 ip address
-export K8SHA_IP3=10.73.71.25
+export K8SHA_IP3=10.73.71.27
 
 
 # master01 hostname
-export K8SHA_HOSTNAME1=kube-master01
+export K8SHA_HOSTNAME1=master01
 
 # master02 hostname
-export K8SHA_HOSTNAME2=kube-master02
+export K8SHA_HOSTNAME2=master02
 
 # master03 hostname
-export K8SHA_HOSTNAME3=kube-master03
+export K8SHA_HOSTNAME3=master03
 
 #etcd tocken:
 export ETCD_TOKEN=9489bf67bdfe1b3ae077d6fd9e7efefd
